5505d2874f7ff6b51e7f12fe3ae28ef2b09c9fcb
Kskefeafyonlu <karsiyakaefe@icloud.com>
2025-09-30 20:28:15 +0300

    CardSystem

---
+++ Assets/_Project/Blacksmithing/Casting/CastDefs.cs
@@ -0,0 +1,7 @@
+namespace _Project.Blacksmithing.Casting
+{
+    public class CastDef
+    {
+        
+    }
+}
---
+++ Assets/_Project/CardSystem/CardData.cs
@@ -0,0 +1,7 @@
+namespace _Project.CardSystem
+{
+    public class CardData
+    {
+        
+    }
+}
